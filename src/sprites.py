b429246bab479078d9f5d11e99d9d28fb1a37540
maulvizm <13522122@std.stei.itb.ac.id>
2025-12-06 20:13:36 +0700

    feat: add basic window and static bird sprite

--- src/sprites.py
+++ src/sprites.py
@@ -1,4 +1,18 @@
 """Sprite classes for the Flappy Bird game."""
 
-# TODO: Add sprite classes here
+import pygame
+from src.constants import (
+    SCREEN_WIDTH,
+    SCREEN_HEIGHT,
+)
 
+
+class Bird(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+
+        self.image = pygame.image.load("assets/sprites/bluebird-upflap.png").convert_alpha()
+
+        self.rect = self.image.get_rect()
+        self.rect[0] = SCREEN_WIDTH / 6
+        self.rect[1] = SCREEN_HEIGHT / 2
