a97981fb2dca36259109d648513698aea5262832
maulvizm <13522122@std.stei.itb.ac.id>
2025-12-06 20:13:36 +0700

    feat: add basic window and static bird sprite

--- main.py
+++ main.py
@@ -1,4 +1,34 @@
 """Main entry point for the Flappy Bird game."""
 
-# TODO: Add game code here
+import pygame
+import sys
+from pygame.locals import *
 
+from src.constants import SCREEN_WIDTH, SCREEN_HEIGHT, FPS
+from src.sprites import Bird
+
+pygame.init()
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Flappy Bird")
+
+BACKGROUND = pygame.image.load("assets/sprites/background-day.png")
+BACKGROUND = pygame.transform.scale(BACKGROUND, (SCREEN_WIDTH, SCREEN_HEIGHT))
+
+bird_group = pygame.sprite.Group()
+bird = Bird()
+bird_group.add(bird)
+
+clock = pygame.time.Clock()
+
+# Main game loop
+while True:
+    clock.tick(FPS)
+    
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+    
+    screen.blit(BACKGROUND, (0, 0))
+    bird_group.draw(screen)
+    pygame.display.update()
